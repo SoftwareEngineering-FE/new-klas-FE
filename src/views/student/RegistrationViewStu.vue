3442542d385ff154efe05d9b8b82a30999d3e27c
angrymusic <angrymusic@naver.com>
2023-06-05 00:12:41 +0900

    #5 강의 검색, 신청된 강의 조회 추가, 각 강의 개별 신청

--- src/views/student/RegistrationViewStu.vue
+++ src/views/student/RegistrationViewStu.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useQuasar } from 'quasar';
-
+const searchText = ref('');
 const q = useQuasar();
 const lectureBasket = ref(new Map());
 type whenType = {
@@ -14,6 +14,7 @@ type lectureType = {
   professor: string;
   when: whenType[];
 };
+const registrationLecture = (lectureId: number) => {};
 const hasTimeOverlap = (time1: number[], time2: number[]) => {
   for (const t1 of time1) {
     for (const t2 of time2) {
@@ -93,12 +94,100 @@ const lectures = [
         time: [0, 1]
       }
     ]
+  },
+  {
+    lectureId: 8459,
+    lectureName: '소프트웨어공학2',
+    professor: '이기훈',
+    when: [
+      {
+        day: 3, // 월 0 화 1 수 2 ...
+        time: [0, 1] // 0교시 1교시
+      },
+      {
+        day: 4,
+        time: [0, 1]
+      }
+    ]
+  },
+  {
+    lectureId: 8459,
+    lectureName: '소프트웨어공학2',
+    professor: '이기훈',
+    when: [
+      {
+        day: 3, // 월 0 화 1 수 2 ...
+        time: [0, 1] // 0교시 1교시
+      },
+      {
+        day: 4,
+        time: [0, 1]
+      }
+    ]
+  },
+  {
+    lectureId: 8459,
+    lectureName: '소프트웨어공학2',
+    professor: '이기훈',
+    when: [
+      {
+        day: 3, // 월 0 화 1 수 2 ...
+        time: [0, 1] // 0교시 1교시
+      },
+      {
+        day: 4,
+        time: [0, 1]
+      }
+    ]
+  },
+  {
+    lectureId: 8459,
+    lectureName: '소프트웨어공학2',
+    professor: '이기훈',
+    when: [
+      {
+        day: 3, // 월 0 화 1 수 2 ...
+        time: [0, 1] // 0교시 1교시
+      },
+      {
+        day: 4,
+        time: [0, 1]
+      }
+    ]
   }
 ];
 </script>
 <template>
   <div class="background">
     <div class="wrapper">
+      <div class="top-board">
+        <div class="title">신청된 강의</div>
+        <table class="q-mb-sm">
+          <colgroup>
+            <col width="10%" />
+            <col width="3%" />
+            <col width="5%" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th class="ths">과목명</th>
+              <th class="ths">교수</th>
+              <th class="ths">시간</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(lecture, index) in lectures" :key="index">
+              <td>{{ lecture.lectureName }}</td>
+              <td>{{ lecture.professor }}</td>
+              <td>
+                <span v-for="(time, tIndex) in lecture.when" :key="tIndex">{{
+                  ' ' + day[time.day] + ' ' + time.time.join(',')
+                }}</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       <div class="row q-mt-md">
         <div
           :class="{
@@ -111,17 +200,19 @@ const lectures = [
             <div class="title">강의 장바구니</div>
             <table class="q-mb-sm">
               <colgroup>
-                <col width="1%" />
+                <col width="7%" />
                 <col width="10%" />
                 <col width="3%" />
                 <col width="5%" />
+                <col width="7%" />
               </colgroup>
               <thead>
                 <tr>
-                  <th class="ths">추가</th>
+                  <th class="ths">제거</th>
                   <th class="ths">과목명</th>
                   <th class="ths">교수</th>
                   <th class="ths">시간</th>
+                  <th class="ths">신청</th>
                 </tr>
               </thead>
               <tbody>
@@ -135,6 +226,15 @@ const lectures = [
                     <span v-for="(time, tIndex) in value.when" :key="tIndex">{{
                       ' ' + day[time.day] + ' ' + time.time.join(',')
                     }}</span>
+                  </td>
+                  <td>
+                    <q-btn
+                      flat
+                      color="kbrown"
+                      icon="check"
+                      dense
+                      @click="registrationLecture(key)"
+                    />
                   </td>
                 </tr>
               </tbody>
@@ -150,37 +250,50 @@ const lectures = [
           }"
         >
           <div class="board">
-            <div class="title">수강 신청</div>
-            <table class="q-mb-sm">
-              <colgroup>
-                <col width="1%" />
-                <col width="10%" />
-                <col width="3%" />
-                <col width="5%" />
-              </colgroup>
-              <thead>
-                <tr>
-                  <th class="ths">추가</th>
-                  <th class="ths">과목명</th>
-                  <th class="ths">교수</th>
-                  <th class="ths">시간</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(lecture, index) in lectures" :key="index">
-                  <td>
-                    <q-btn flat color="kbrown" icon="add" dense @click="addLecture(lecture)" />
-                  </td>
-                  <td>{{ lecture.lectureName }}</td>
-                  <td>{{ lecture.professor }}</td>
-                  <td>
-                    <span v-for="(time, tIndex) in lecture.when" :key="tIndex">{{
-                      ' ' + day[time.day] + ' ' + time.time.join(',')
-                    }}</span>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <div class="title">강의 조회</div>
+            <div class="row q-my-sm">
+              <q-input
+                outlined
+                class="col-8"
+                color="kbrown"
+                v-model="searchText"
+                placeholder="교수명 / 강의명 검색"
+                dense
+              />
+              <q-btn class="q-mx-sm" color="kbrown" icon="search" />
+            </div>
+            <div class="table">
+              <table class="q-mb-s">
+                <colgroup>
+                  <col width="1%" />
+                  <col width="10%" />
+                  <col width="3%" />
+                  <col width="5%" />
+                </colgroup>
+                <thead>
+                  <tr>
+                    <th class="ths">추가</th>
+                    <th class="ths">과목명</th>
+                    <th class="ths">교수</th>
+                    <th class="ths">시간</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(lecture, index) in lectures" :key="index">
+                    <td>
+                      <q-btn flat color="kbrown" icon="add" dense @click="addLecture(lecture)" />
+                    </td>
+                    <td>{{ lecture.lectureName }}</td>
+                    <td>{{ lecture.professor }}</td>
+                    <td>
+                      <span v-for="(time, tIndex) in lecture.when" :key="tIndex">{{
+                        ' ' + day[time.day] + ' ' + time.time.join(',')
+                      }}</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
@@ -201,7 +314,14 @@ const lectures = [
   border: 1px solid #d1d1d1;
   border-radius: 5px;
   padding: 15px;
-  min-height: 300px;
+  min-height: 500px;
+}
+.top-board {
+  background-color: #fafafa;
+  border: 1px solid #d1d1d1;
+  border-radius: 5px;
+  padding: 15px;
+  min-height: 100px;
 }
 .title {
   font-size: 18px;
@@ -211,9 +331,8 @@ const lectures = [
   width: 200px;
 }
 .table {
-  width: 100%;
-  border-right: 1px solid #d1d1d1;
-  border-bottom: 1px solid #d1d1d1;
+  max-height: 360px;
+  overflow-y: scroll;
 }
 .first {
   width: 20px;
